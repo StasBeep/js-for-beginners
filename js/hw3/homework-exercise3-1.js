41c54946013660a1d8f2fdc1433bd804efb1a2cc
StasBeep <stas-stas.stanislav@mail.ru>
2021-04-02 13:23:35 +0300

    Изменение доп материалов

--- js/hw3/homework-exercise3-1.js
+++ js/hw3/homework-exercise3-1.js
@@ -12,11 +12,9 @@
 for (let i = 0; i <= 10; i++) {
     if (i == 0) {
         console.log(i + " - это ноль");
+    } else if (i % 2 == 0) {
+        console.log(i + " - чётное число");
     } else {
-        if(i % 2 == 0){
-            console.log(i + " - чётное число");
-        }else{
-            console.log(i + " - нечётное число");
-        }
+        console.log(i + " - нечётное число");
     }
 }
